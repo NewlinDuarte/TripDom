f289e1ab7f5ebdca35b94cd290c2108bfe9730f0
MESCyT <dnewlin13@hotmail.com>
2017-11-26 15:25:12 -0400

    I think first cmd push to Santiago branch, changes to UsuarioDbo

--- app/src/main/java/edu/itla/tripdom/dao/UsuarioDbo.java
+++ app/src/main/java/edu/itla/tripdom/dao/UsuarioDbo.java
@@ -2,6 +2,7 @@ package edu.itla.tripdom.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.util.List;
 
@@ -18,15 +19,24 @@ public class UsuarioDbo {
         connection = new DbConnection(context);
     }
 
+    //Creates a "Usuario" in the "usuario" table in the database
     public void crear(Usuario usuario)
     {
         connection.getWritableDatabase();
         ContentValues cv = new ContentValues();
 
+        //Campos para insertar en usuario
         cv.put("nombre", usuario.getNombre());
         cv.put("email", usuario.getEmail());
         cv.put("telefono", usuario.getTelefono());
         cv.put("tipo_usuario", usuario.getTipo().toString());
+
+        //abriendo db para insertar en Usuario
+        SQLiteDatabase db = connection.getWritableDatabase();
+        db.insert("usuario", null, cv);
+
+        //Cerrando Db
+        db.close();
     }
 
     public List<Usuario> buscar()
