6417e928c9e674bfa6d9eb7bda42074453839ae0
dnewlin13@hotmail.com <dnewlin13@hotmail.com>
2017-11-30 15:05:21 -0400

    Adding PublicacionDbo and Publicacion,Publicacion_Detalle script to SqlHelperSchema.....Gradle Build Succesful

--- app/src/main/java/edu/itla/tripdom/dao/SqlHelperSchema.java
+++ app/src/main/java/edu/itla/tripdom/dao/SqlHelperSchema.java
@@ -4,8 +4,7 @@ package edu.itla.tripdom.dao;
  * Created by MESCyT on 26/11/2017.
  */
 
-public class SqlHelperSchema
-{
+public class SqlHelperSchema {
     public static final String USUARIO_TABLE = "CREATE TABLE IF NOT EXISTS `usuario` (" +
             "`id` INTEGER PRIMARY KEY AUTOINCREMENT," +
             "`nombre` TEXT NOT NULL," +
@@ -14,4 +13,28 @@ public class SqlHelperSchema
             "`tipo_usuario` TEXT NOT NULL" +
             ");";
     //Crear las restantes entidades
+
+    public static final String PUBLICACION_TABLE = "CREATE TABLE IF NOT EXISTS `publicacion` (" +
+            "`id`	INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "`fecha`	TEXT NOT NULL,         " +
+            "`fechaViaje`	TEXT NOT NULL," +
+            "`costo`	REAL NOT NULL," +
+            "`descripcion`	TEXT NOT NULL," +
+            "`status`	TEXT NOT NULL," +
+            "`usuario_id`	INTEGER NOT NULL," +
+            "`origen`	TEXT NOT NULL," +
+            "`cupo`	INTEGER" +
+            ");";
+
+    public static final String PUBLICACION_DETALLE_TABLE = "CREATE TABLE IF NOT EXISTS `publicacion_detalle` (" +
+            "`id`INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "`publicacion_id`INTEGER NOT NULL," +
+            "`lugar`TEXT NOT NULL," +
+            "`descripcion`TEXT NOT NULL" +
+            ");";
+
+
+
+
+
 }
--- app/src/main/java/edu/itla/tripdom/dao/TipoUsuarioDbo.java
+++ app/src/main/java/edu/itla/tripdom/dao/TipoUsuarioDbo.java
@@ -1,8 +1,15 @@
 package edu.itla.tripdom.dao;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
+import edu.itla.tripdom.entity.Usuario;
+
 /**
  * Created by Newlin on 11/30/2017.
  */
 
-public class TipoUsuarioDbo {
+public class TipoUsuarioDbo
+{
+
 }
